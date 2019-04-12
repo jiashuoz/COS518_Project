afe4b1759fb45a2a206300208f981031683b0777
Jiashuo Zhang <jiashuoz@outlook.com>
2019-04-12 15:10:00 -0400

    fixed gitignore; chord_server.go: added FindPredecessor func; test: added test func

--- src/chord/chord_server.go
+++ src/chord/chord_server.go
@@ -4,14 +4,36 @@ import (
 	"math/big"
 )
 
-type ChordServer struct {
+// Server is a single ChordServer
+type Server struct {
 	name   string
 	ipAddr string
 	node   *Node
 }
 
+// FindPredecessor returns the previous node in the circle to id
+func (chordServer *Server) FindPredecessor(id []byte) *NodeInfo {
+	currNode := chordServer.node
+	if betweenRightInclusive(id, currNode.me.id, currNode.successor.id) {
+		return currNode.me
+	}
+	return chordServer.closestPrecedingFinger(id)
+}
+
+func betweenRightInclusive(target []byte, begin []byte, end []byte) bool {
+	targetBigInt := big.NewInt(0).SetBytes(target)
+	beginBigInt := big.NewInt(0).SetBytes(begin)
+	endBigInt := big.NewInt(0).SetBytes(end)
+
+	if beginBigInt.Cmp(endBigInt) == 1 { // begin > end, (3, 0]
+		return targetBigInt.Cmp(beginBigInt) == 1 || targetBigInt.Cmp(endBigInt) == -1 || targetBigInt.Cmp(endBigInt) == 0
+	}
+	// begin < end, (2, 3]
+	return targetBigInt.Cmp(beginBigInt) == 1 && (targetBigInt.Cmp(endBigInt) == -1 || targetBigInt.Cmp(endBigInt) == 0)
+}
+
 // Returns the closest node preceding id: previous node in the circle to id
-func (chordServer *ChordServer) closestPrecedingFinger(id []byte) *NodeInfo {
+func (chordServer *Server) closestPrecedingFinger(id []byte) *NodeInfo {
 	// slice contains pointers to TableEntry
 	fingerTable := chordServer.node.FingerTable()
 	n := chordServer.node.ID()
--- test/chord_server_test.go
+++ test/chord_server_test.go
@@ -5,3 +5,7 @@ import "testing"
 func TestBetween(t *testing.T) {
 	// TODO: test between function in chord_server.go
 }
+
+func TestBetweenRightInclusive(t *testing.T) {
+	// TODO: test betweenRightInclusive function in chord_server.go
+}
